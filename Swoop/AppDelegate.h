e7592035558c245adeece447760d38b42aca3c30
Brian Leonard <bleonard@hmc.edu>
2015-01-01 00:02:17 -0800

    Initial Commit

---
+++ Swoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swoop
+//
+//  Created by Brian on 1/1/15.
+//  Copyright (c) 2015 Brian Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
